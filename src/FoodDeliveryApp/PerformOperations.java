f057e3ad1cd29c68c3e816c6739dc9ce6080852d
Gözde Kurtulmuş <gozdekrtlms@gmail.com>
2021-05-22 20:04:52 +0300

    Update PerformOperations.java

--- src/FoodDeliveryApp/PerformOperations.java
+++ src/FoodDeliveryApp/PerformOperations.java
@@ -3,13 +3,26 @@ package FoodDeliveryApp;
 public class PerformOperations {
     private StockBst stock ;
     private FileIO fileIO = new FileIO();
+    private BinaryTree<Restaurant> ratingTree;
+    private BinaryTree<Restaurant> deliveryTree;
+    private BinaryTree<Food> priceTree;
+    private BinaryTree<Food> stockTree;
 
     public PerformOperations(){
-        stock = new StockBst(fileIO.readToArray("CENG112_HW4.csv"));
+        this.stock = new StockBst(fileIO.readToArray("CENG112_HW4.csv"));
+        this.ratingTree = stock.getRatingTree();
+        this.deliveryTree = stock.getDeliveryTree();
+        this.priceTree = stock.getPriceTree();
+        this.stockTree = stock.getStockTree();
+    }
+
+    public void start(){
+        operation1();
+
     }
 
     public void operation1(){
-        BinaryTree<Restaurant> temp = stock.getRatingTree();
+        BinaryTree<Restaurant> temp = ratingTree;
         ArrayStack<Restaurant> stack = turnIntoDescending(temp.traverseAscending());
 
         Restaurant element = stack.pop();
@@ -18,6 +31,12 @@ public class PerformOperations {
             element = stack.pop();
         }
     }
+
+    public void operation2(){
+
+
+    }
+
 
     public ArrayStack<Restaurant> turnIntoDescending(ArrayQueue<Restaurant> queue){
         ArrayStack<Restaurant> stack = new ArrayStack<>();
@@ -30,3 +49,4 @@ public class PerformOperations {
     }
 
 }
+

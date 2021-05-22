55f2e8d0213860d6b1559b18c32a4f8299942b28
MerveMalak <73018652+MerveMalak@users.noreply.github.com>
2021-05-22 14:36:02 +0300

    Add files via upload

---
+++ src/FoodDeliveryApp/BinaryTree.java
@@ -0,0 +1,41 @@
+package FoodDeliveryApp;
+
+public class BinaryTree<T> {
+    private class BinaryNode<T>{
+
+        private T data;
+        private BinaryNode<T> leftChild;
+        private  BinaryNode<T> rightChild;
+
+        public BinaryNode(T data){
+            this.data = data;
+        }
+        private BinaryNode(T data, BinaryNode<T> leftChild, BinaryNode<T> rightChild){
+            this.data = data;
+            this.leftChild = leftChild;
+            this.rightChild = rightChild;
+        }
+        private T getData(){
+            return data;
+        }
+        private void setData(T data){
+            this.data = data;
+        }
+        private BinaryNode getLeftChild(){
+            return this.leftChild;
+        }
+        private void setLeftChild(BinaryNode<T> leftChild){
+            this.leftChild = leftChild;
+        }
+        private BinaryNode getRightChild(){
+            return this.rightChild;
+        }
+        private void setRightChild(BinaryNode<T> rightChild) {
+            this.rightChild = rightChild;
+        }
+    }
+
+
+
+
+}

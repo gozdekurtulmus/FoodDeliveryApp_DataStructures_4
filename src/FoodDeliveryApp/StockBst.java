200ae8ee8050b656851316814aa4f1af0d16b968
Gözde Kurtulmuş <gozdekrtlms@gmail.com>
2021-05-22 18:26:29 +0300

    Update StockBst.java

--- src/FoodDeliveryApp/StockBst.java
+++ src/FoodDeliveryApp/StockBst.java
@@ -1,14 +1,13 @@
 package FoodDeliveryApp;
 
 public class StockBst {
-    private BinaryTree<Food> priceTree = new BinaryTree(); ;
+    private BinaryTree<Food> priceTree ;
     private BinaryTree<Food> stockTree ;
     private BinaryTree<Restaurant> ratingTree ;
     private BinaryTree<Restaurant> deliveryTree;
 
     private Food[] foodArray ;
     private Restaurant[] restaurantArray;
-
 
     private String[][] propertyList ;
     private ArrayIterator<String[]> arrayIterator ;
@@ -23,6 +22,11 @@ public class StockBst {
         int listLength = propertyList.length;
         arrayIterator = new ArrayIterator<>(propertyList, listLength);
 
+        this.priceTree = new BinaryTree<>();
+        this.stockTree = new BinaryTree<>();
+        this.ratingTree = new BinaryTree<>();
+        this.deliveryTree= new BinaryTree<>();
+
         this.foodArray = new Food[listLength];
         this.restaurantArray = new Restaurant[listLength];
 
@@ -32,43 +36,41 @@ public class StockBst {
 
         createFoodTrees();
         createRestaurantTrees();
-
-        this.stockTree = new BinaryTree();
-        this.ratingTree = new BinaryTree();
-        this.deliveryTree= new BinaryTree();
-
     }
 
     private void createFoodTrees(){
-        ArrayIterator<Food> iterator = new ArrayIterator<>(foodArray, propertyList.length);
+        ArrayIterator<Food> iterator = new ArrayIterator<>(foodArray, foodArray.length);
 
-        while(iterator.hasNext()){
-            Food element= iterator.next();
-            priceTree.add(element, element.getPrice());
-        }
-        arrayIterator.setCurrent(0);
-        while(iterator.hasNext()){
-            Food element= iterator.next();
-            priceTree.add(element, element.getStock());
-        }
-    }
-
-    private void createRestaurantTrees(){
-        ArrayIterator<Restaurant> iterator = new ArrayIterator<>(restaurantArray, propertyList.length);
-
-        while(iterator.hasNext()){
-            Restaurant element= iterator.next();
-            ratingTree.add(element, element.getRating());
-        }
-        arrayIterator.setCurrent(0);
-        while(iterator.hasNext()){
-            Restaurant element= iterator.next();
-            deliveryTree.add(element, element.getDeliveryTime());
+        for(int i=0; i<2; i++){
+            while(iterator.hasNext()){
+                Food element= iterator.next();
+                if(i == 0){
+                    priceTree.add(element, element.getPrice());
+                }
+                else{
+                    stockTree.add(element, element.getStock());
+                }
+            }
+            iterator.setCurrent(0);
         }
     }
 
+    private void createRestaurantTrees() {
+        ArrayIterator<Restaurant> iterator = new ArrayIterator<>(restaurantArray, restaurantArray.length);
 
-
+        for (int i = 0; i < 2; i++) {
+            while (iterator.hasNext()) {
+                Restaurant element = iterator.next();
+                if (element != null) {
+                    if (i == 0) {
+                        ratingTree.add(element, element.getRating());
+                    } else {
+                        deliveryTree.add(element, element.getDeliveryTime());
+                    }
+                }
+            }
+        }
+    }
 
     private void createFoodArray(){
         arrayIterator.next();
@@ -94,7 +96,38 @@ public class StockBst {
                this.restaurantArray[arrayIterator.getCurrent()-1] = nextElement;
             }
         }
+    }
 
+    public BinaryTree<Food> getPriceTree() {
+        return priceTree;
+    }
+
+    public void setPriceTree(BinaryTree<Food> priceTree) {
+        this.priceTree = priceTree;
+    }
+
+    public BinaryTree<Food> getStockTree() {
+        return stockTree;
+    }
+
+    public void setStockTree(BinaryTree<Food> stockTree) {
+        this.stockTree = stockTree;
+    }
+
+    public BinaryTree<Restaurant> getRatingTree() {
+        return ratingTree;
+    }
+
+    public void setRatingTree(BinaryTree<Restaurant> ratingTree) {
+        this.ratingTree = ratingTree;
+    }
+
+    public BinaryTree<Restaurant> getDeliveryTree() {
+        return deliveryTree;
+    }
+
+    public void setDeliveryTree(BinaryTree<Restaurant> deliveryTree) {
+        this.deliveryTree = deliveryTree;
     }
 
 

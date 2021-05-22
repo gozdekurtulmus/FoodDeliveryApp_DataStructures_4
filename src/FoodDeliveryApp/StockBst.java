4950a276db4bc3eb4acf6f4f13539822bb812f3a
Gözde Kurtulmuş <gozdekrtlms@gmail.com>
2021-05-22 17:35:05 +0300

    Update StockBst.java

--- src/FoodDeliveryApp/StockBst.java
+++ src/FoodDeliveryApp/StockBst.java
@@ -31,6 +31,7 @@ public class StockBst {
         createRestaurantArray();
 
         createFoodTrees();
+        createRestaurantTrees();
 
         this.stockTree = new BinaryTree();
         this.ratingTree = new BinaryTree();

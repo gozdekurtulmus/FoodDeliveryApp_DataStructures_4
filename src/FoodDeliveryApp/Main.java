be83da3ab4700e3413e096d898f254961b094434
Gözde Kurtulmuş <gozdekrtlms@gmail.com>
2021-05-28 01:58:10 +0300

    Update Main.java

--- src/FoodDeliveryApp/Main.java
+++ src/FoodDeliveryApp/Main.java
@@ -1,5 +1,12 @@
 package FoodDeliveryApp;
 
+/** This class has the main method of the Food Delivery Application.
+ * It calls PerformOperations class to start the simulation.
+ *
+ * @author Merve Malak     260201043
+ * @author Gozde Kurtulmus 280201105
+ */
+
 public class Main {
 
     public static void main(String[] args) {

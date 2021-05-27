d512f2d55287c7237af689bb48d71c0c8c94d517
Gözde Kurtulmuş <gozdekrtlms@gmail.com>
2021-05-28 02:02:10 +0300

    Update ArrayIterator.java

--- src/FoodDeliveryApp/ArrayIterator.java
+++ src/FoodDeliveryApp/ArrayIterator.java
@@ -3,8 +3,8 @@ package FoodDeliveryApp;
 import java.util.NoSuchElementException;
 
 public class ArrayIterator<T> implements Iterator<T>{
-    private int count;
-    private int current;
+    private int count; //length of the array
+    private int current; //Current index
     private T[] items;
 
     public ArrayIterator(T[] collection, int size){
@@ -13,22 +13,24 @@ public class ArrayIterator<T> implements Iterator<T>{
         current = 0;
     }
 
+    //Returns true if the Array still has elements.
     public boolean hasNext(){
         return current<count;
     }
 
+    //Returns the next element, throws NoSuchElementException if the array doesn't have next element.
     public T next(){
-        if(! hasNext()){
+        if(!hasNext()){
             throw new NoSuchElementException();
         }
         current++;
         return items[current-1];
     }
 
+    //getter and setters
     public int getCurrent(){
         return current;
     }
-
     public void setCurrent(int value){
         this.current = value;
     }

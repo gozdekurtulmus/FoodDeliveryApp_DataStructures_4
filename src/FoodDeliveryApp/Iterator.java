f4c9bcb1353436654d3cf329f70e220496249f97
Gözde Kurtulmuş <gozdekrtlms@gmail.com>
2021-05-30 13:07:42 +0300

    Update Iterator.java

--- src/FoodDeliveryApp/Iterator.java
+++ src/FoodDeliveryApp/Iterator.java
@@ -1,6 +1,16 @@
 package FoodDeliveryApp;
 
 public interface Iterator<T> {
+    /**
+     * Looks if there are more elements.
+     * @return true if there are more elements in the collection, false otherwise.
+     */
     boolean hasNext();
+
+    /**
+     * @return next element of the collection.
+     * @throws Error if the element does not exist.
+     */
     T next();
 }
+

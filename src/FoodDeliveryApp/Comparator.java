07fac833c0cd76495559dc0e33e93230dbdfbac4
Gözde Kurtulmuş <gozdekrtlms@gmail.com>
2021-05-30 17:00:42 +0300

    Update Comparator.java

--- src/FoodDeliveryApp/Comparator.java
+++ src/FoodDeliveryApp/Comparator.java
@@ -1,5 +1,12 @@
 package FoodDeliveryApp;
 
 public interface Comparator<T> {
-    public double compare(T first, T second);
+    
+    /**
+     * Specifies a comparison
+     * @param first element to compare
+     * @param second element to be compared with
+     * @return positive if first is bigger, 0 if equal and negative otherwise.
+     */
+    double compare(T first, T second);
 }

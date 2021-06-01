98723a86f40f4ecccb3624782b14f0e0afaaa03a
MerveMalak <73018652+MerveMalak@users.noreply.github.com>
2021-06-01 21:30:36 +0300

    Update StackInterface.java

--- src/FoodDeliveryApp/StackInterface.java
+++ src/FoodDeliveryApp/StackInterface.java
@@ -1,37 +1,32 @@
 package FoodDeliveryApp;
 
 public interface StackInterface<T>{
-    /*Adds a new entry to the top of the stack.
+    /**Adds a new entry to the top of the stack.
     @param: newEntry an object to be added to the stack.*/
     void push(T newEntry);
 
-    /*Removes and returns this stack's top entry.
+    /**Removes and returns this stack's top entry.
     @param: none.
-    @return The object at the top of the stack.
-     */
+    @return The object at the top of the stack.*/
     T pop();
 
-    /* Retrieves this stack's top entry.
+    /**Retrieves this stack's top entry.
     @param:none.
-    @return: The object at the top of the stack.
-     */
+    @return: The object at the top of the stack.*/
     T peek();
 
-    /*Detects whether this stack is empty.
+    /**Detects whether this stack is empty.
     @param: none.
-    @return True if the stack is empty,or false otherwise.
-     */
+    @return True if the stack is empty,or false otherwise.*/
     boolean isEmpty();
 
-    /*Removes all entries from this stack.
-    @param: none.
-     */
+    /**Removes all entries from this stack.
+    @param: none.*/
     void clear();
 
-    /* Shows how many entries stack has.
+    /**Shows how many entries stack has.
     @param: none
-    @return int entry counts.
-     */
+    @return int entry counts.*/
     int getLength();
 
 }

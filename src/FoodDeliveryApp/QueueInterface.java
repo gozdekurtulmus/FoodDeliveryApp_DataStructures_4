07da7e23c31d2efa5c857998f02217cda0869f3a
MerveMalak <73018652+MerveMalak@users.noreply.github.com>
2021-06-01 21:07:55 +0300

    Update QueueInterface.java

--- src/FoodDeliveryApp/QueueInterface.java
+++ src/FoodDeliveryApp/QueueInterface.java
@@ -2,25 +2,24 @@ package FoodDeliveryApp;
 
 public interface QueueInterface<T> {
 
-    /*Detects whether this queue is full
+    /**Detects whether this queue is full
     @param: none
     @return: True if the queue is full, or false otherwise.*/
     boolean isFull();
 
-    /*Detects whether this queue is empty
+    /**Detects whether this queue is empty
     @param: none
     @return: True if the queue is empty, or false otherwise.*/
     boolean isEmpty();
 
-    /* Adds a new entry to the back of this queue.
+    /** Adds a new entry to the back of this queue.
     @param: newEntry an object to be added.
     @return: True if the newEntry is added, or false otherwise.*/
     boolean enqueue(T newEntry);
 
-    /*Removes and returns the entry at the front of this queue.
+    /** Removes and returns the entry at the front of this queue.
     @param: none
-    @return: the object at the front of the queue.
-     */
+    @return: the object at the front of the queue.*/
     T dequeue();
 
 }

f1f779349d7e78ad0cb6befaae52e01d0f19e51b
MerveMalak <73018652+MerveMalak@users.noreply.github.com>
2021-06-01 20:22:34 +0300

    Update Orderable.java

--- src/FoodDeliveryApp/Orderable.java
+++ src/FoodDeliveryApp/Orderable.java
@@ -1,7 +1,17 @@
 package FoodDeliveryApp;
 
 public interface Orderable {
+    
+    /** 
+     Updates the price of the foods
+    @param: the price of the food 
+     */ 
     void updatePrice(double price);
-    void updateStock(int stock);
+
+    /**
+     Updates the stock of the foods
+     @param: the stock of the food
+     */
+    void updateStock(int stock);  
 
 }

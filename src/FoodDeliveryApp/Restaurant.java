9a49e5da03c82bcaf9ba5ae1a3bc2c148630177c
MerveMalak <73018652+MerveMalak@users.noreply.github.com>
2021-06-01 21:06:46 +0300

    Update Restaurant.java

--- src/FoodDeliveryApp/Restaurant.java
+++ src/FoodDeliveryApp/Restaurant.java
@@ -1,12 +1,12 @@
 package FoodDeliveryApp;
 
 public class Restaurant {
-    private String name;
-    private double rating;
-    private String cuisine;
+    private String name; //keeps the name of the restaurant
+    private double rating; //keeps the rate of the restaurant
+    private String cuisine; //keeps the cuisine of the restaurant
     private int deliveryTime; // in minutes
 
-
+    //constructor
     public Restaurant(String[] properties) {
         this.name = properties[3];
         this.rating = Double.parseDouble(properties[4]);
@@ -14,19 +14,7 @@ public class Restaurant {
         this.deliveryTime = Integer.parseInt(properties[6]);
     }
 
-
-    public void updateCuisine(String category) {
-        this.cuisine = category;
-    }
-
-    public void updateRating(double score) {
-        this.rating = score;
-    }
-
-    public void updateDeliveryTime(int deliveryTime) {
-        this.deliveryTime = deliveryTime;
-    }
-
+    //starts the getters and setters methods
     public String getName() {
         return name;
     }
@@ -58,18 +46,27 @@ public class Restaurant {
     public void setDeliveryTime(int deliveryTime) {
         this.deliveryTime = deliveryTime;
     }
-
+    //ends the setters and getters methods
 }
+/**
+ * Places nodes in BST according to the rate of the restaurants
+ */
 class RatingComparator implements Comparator<Restaurant>{
 
     @Override
+    //@return positive if first is bigger, 0 if equal and negative otherwise.
     public double compare(Restaurant first, Restaurant second) {
         return first.getRating() - second.getRating();
     }
 }
+
+/**
+ * Places nodes in BST according to the delivery time of the restaurants
+ */
 class DeliveryTimeComparator implements Comparator<Restaurant>{
 
     @Override
+    //@return positive if first is bigger, 0 if equal and negative otherwise.
     public double compare(Restaurant first, Restaurant second) {
         return first.getDeliveryTime()-second.getDeliveryTime();
     }

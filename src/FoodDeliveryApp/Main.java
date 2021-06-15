c299cede1fff583b605b6d1d3a36e31c25fed75f
Gözde Kurtulmuş <gozdekrtlms@gmail.com>
2021-06-15 22:16:47 +0300

    Update Main.java

--- src/FoodDeliveryApp/Main.java
+++ src/FoodDeliveryApp/Main.java
@@ -12,9 +12,9 @@ public class Main {
     public static void main(String[] args) {
 
         PerformOperations operation = new PerformOperations();
-        operation.operation1();
-
+        operation.start();
 
     }
 
+}
 }
